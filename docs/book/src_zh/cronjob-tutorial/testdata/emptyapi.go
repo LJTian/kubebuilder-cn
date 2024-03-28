89a99600c2b932ae8daf030ca74445d444bc9ef8
ljtian <tianlijun315@gmail.com>
2024-03-28 14:19:13 +0800

    1、翻译 cronjob-new-api

--- docs/book/src_zh/cronjob-tutorial/testdata/emptyapi.go
+++ docs/book/src_zh/cronjob-tutorial/testdata/emptyapi.go
@@ -1,24 +1,20 @@
 /*
-Copyright 2022.
+版权所有 2022。
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+根据 Apache 许可证 2.0 版（"许可证"）获得许可；
+除非符合许可证的规定，否则您不得使用此文件。
+您可以在下面的网址获取许可证的副本
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+除非适用法律要求或书面同意，否则根据许可证分发的软件
+以"原样"为基础分发，没有任何明示或暗示的保证或条件。
+请参阅特定语言管理权限和限制的许可证。
 */
 // +kubebuilder:docs-gen:collapse=Apache License
 
 /*
-We start out simply enough: we import the `meta/v1` API group, which is not
-normally exposed by itself, but instead contains metadata common to all
-Kubernetes Kinds.
+我们从简单的开始：我们导入 `meta/v1` API 组，它通常不是单独公开的，而是包含所有 Kubernetes Kind 的公共元数据。
 */
 package v1
 
@@ -27,52 +23,40 @@ import (
 )
 
 /*
-Next, we define types for the Spec and Status of our Kind.  Kubernetes functions
-by reconciling desired state (`Spec`) with actual cluster state (other objects'
-`Status`) and external state, and then recording what it observed (`Status`).
-Thus, every *functional* object includes spec and status.  A few types, like
-`ConfigMap` don't follow this pattern, since they don't encode desired state,
-but most types do.
+接下来，我们为我们的 Kind 的 Spec 和 Status 定义类型。Kubernetes 通过协调期望的状态（`Spec`）与实际的集群状态（其他对象的`Status`）和外部状态，然后记录它观察到的内容（`Status`）来运行。因此，每个 *functional* 对象都包括 spec 和 status。一些类型，比如 `ConfigMap` 不遵循这种模式，因为它们不编码期望的状态，但大多数类型都是这样。
 */
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// 编辑此文件！这是你拥有的脚手架！
+// 注意：json 标记是必需的。您添加的任何新字段必须具有 json 标记，以便对字段进行序列化。
 
-// CronJobSpec defines the desired state of CronJob
+// CronJobSpec 定义了 CronJob 的期望状态
 type CronJobSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// 插入其他的 Spec 字段 - 集群的期望状态
+	// 重要提示：在修改此文件后运行 "make" 以重新生成代码
 }
 
-// CronJobStatus defines the observed state of CronJob
+// CronJobStatus 定义了 CronJob 的观察状态
 type CronJobStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// 插入其他的状态字段 - 定义集群的观察状态
+	// 重要提示：在修改此文件后运行 "make" 以重新生成代码
 }
 
 /*
-Next, we define the types corresponding to actual Kinds, `CronJob` and `CronJobList`.
-`CronJob` is our root type, and describes the `CronJob` kind.  Like all Kubernetes objects, it contains
-`TypeMeta` (which describes API version and Kind), and also contains `ObjectMeta`, which holds things
-like name, namespace, and labels.
+接下来，我们定义与实际 Kinds 对应的类型，`CronJob` 和 `CronJobList`。
+`CronJob` 是我们的根类型，描述了 `CronJob` 类型。与所有 Kubernetes 对象一样，它包含 `TypeMeta`（描述 API 版本和 Kind），
+还包含 `ObjectMeta`，其中包含名称、命名空间和标签等信息。
 
-`CronJobList` is simply a container for multiple `CronJob`s.  It's the Kind used in bulk operations,
-like LIST.
+`CronJobList` 简单地是多个 `CronJob` 的容器。它是用于批量操作（如 LIST）的 Kind。
 
-In general, we never modify either of these -- all modifications go in either Spec or Status.
+一般情况下，我们从不修改它们中的任何一个 -- 所有的修改都在 Spec 或 Status 中进行。
 
-That little `+kubebuilder:object:root` comment is called a marker.  We'll see
-more of them in a bit, but know that they act as extra metadata, telling
-[controller-tools](https://github.com/kubernetes-sigs/controller-tools) (our code and YAML generator) extra information.
-This particular one tells the `object` generator that this type represents
-a Kind.  Then, the `object` generator generates an implementation of the
-[runtime.Object](https://pkg.go.dev/k8s.io/apimachinery/pkg/runtime?tab=doc#Object) interface for us, which is the standard
-interface that all types representing Kinds must implement.
+这个小小的 `+kubebuilder:object:root` 注释称为标记。我们稍后会看到更多这样的标记，但要知道它们作为额外的元数据，告诉 [controller-tools](https://github.com/kubernetes-sigs/controller-tools)（我们的代码和 YAML 生成器）额外的信息。
+这个特定的标记告诉 `object` 生成器，这个类型表示一个 Kind。然后，`object` 生成器为我们生成了 [runtime.Object](https://pkg.go.dev/k8s.io/apimachinery/pkg/runtime?tab=doc#Object) 接口的实现，这是所有表示 Kinds 的类型必须实现的标准接口。
 */
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 
-// CronJob is the Schema for the cronjobs API
+// CronJob 是 cronjobs API 的架构
 type CronJob struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -83,7 +67,7 @@ type CronJob struct {
 
 //+kubebuilder:object:root=true
 
-// CronJobList contains a list of CronJob
+// CronJobList 包含 CronJob 的列表
 type CronJobList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
@@ -91,8 +75,7 @@ type CronJobList struct {
 }
 
 /*
-Finally, we add the Go types to the API group.  This allows us to add the
-types in this API group to any [Scheme](https://pkg.go.dev/k8s.io/apimachinery/pkg/runtime?tab=doc#Scheme).
+最后，我们将 Go 类型添加到 API 组中。这使我们可以将此 API 组中的类型添加到任何 [Scheme](https://pkg.go.dev/k8s.io/apimachinery/pkg/runtime?tab=doc#Scheme) 中。
 */
 func init() {
 	SchemeBuilder.Register(&CronJob{}, &CronJobList{})
